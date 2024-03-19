e5cd6d29e9f4e7e759163e55a14c96fbdab1d798
chim347 <nhanntse160792@gmail.com>
2024-03-20 03:44:59 +0700

    update admin

---
+++ Source-Backend/LinhChiDoiSOS.API/Controllers/Admin/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using LinhChiDoiSOS.Application.Common.Response;
+using LinhChiDoiSOS.Application.Features.Customers.Commands.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinhChiDoiSOS.API.Controllers.Admin
+{
+    [Route("api/v1/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public AdminController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public Task<SOSResponse> Post(CreateCustomerCommand request)
+        {
+            var response = _mediator.Send(request);
+            return response;
+        }
+    }
+}
---
+++ Source-Backend/LinhChiDoiSOS.Application/Features/Admins/Commands/CreateAdminCommand.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using LinhChiDoiSOS.Application.Common.Exceptions;
+using LinhChiDoiSOS.Application.Common.Interfaces;
+using LinhChiDoiSOS.Application.Common.Response;
+using LinhChiDoiSOS.Domain.Entities;
+using LinhChiDoiSOS.Domain.IdentityModels;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinhChiDoiSOS.Application.Features.Admins.Commands
+{
+    public class CreateAdminCommand : IRequest<SOSResponse>
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Fullname { get; set; }
+        public string? Phone { get; set; }
+    }
+
+    public class CreateAdminCommandHandler : IRequestHandler<CreateAdminCommand, SOSResponse>
+    {
+        private ILinhChiDoiSOSDbContext _dbContext { get; set; }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IMapper _mapper;
+
+        public CreateAdminCommandHandler(
+            ILinhChiDoiSOSDbContext dbContext,
+            IMapper mapper,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public async Task<SOSResponse> Handle(CreateAdminCommand request, CancellationToken cancellationToken)
+        {
+            var emailExist = await _userManager.FindByEmailAsync(request.Email);
+            if (emailExist != null)
+            {
+                throw new BadRequestException("Customer already exists!");
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                Fullname = request.Fullname,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                PhoneNumber = request.Phone,
+                EmailConfirmed = false,
+                Address = null,
+                BirthDay = null,
+                Image = "https://firebasestorage.googleapis.com/v0/b/linhchidoisos.appspot.com/o/avatar%2Fuser132.jpg?alt=media&token=70eb3ce5-4145-4b18-b974-1b741fecba4c",
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+                await _signInManager.SignInAsync(user, isPersistent: false);
+            }
+            else
+            {
+                throw new BadRequestException("Account creation failed !");
+            }
+            var admin = new Admin
+            {
+                ApplicationUserId = user.Id,
+                IsDelete = false,
+            };
+
+            await _dbContext.Admin.AddAsync(admin);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return new SOSResponse
+            {
+                Message = "Create Successfully"
+            };
+        }
+    }
+}
